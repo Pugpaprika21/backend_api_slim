21fbff90ffbbd4de4bbb4a6e5c60a7dddf8c96d9
Pugpaprika21 <pugpaprika21@gmail.com>
2023-08-07 07:12:25 -0700

    E

--- router/api.php
+++ router/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Slim\Routing\RouteCollectorProxy;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '../../src/dependency.php';
@@ -24,15 +24,12 @@ $app->add(function (Request $request, RequestHandler $handler): Response {
         ->withHeader('Access-Control-Allow-Methods', '*');
 });
 
-enum API: string
-{
-    const Token = '79f5b6d5e8c3280e5db1d5bda60c46232b2c858bf3dd060b0cc065a83f394b27';
-}
+const TOKEN = '79f5b6d5e8c3280e5db1d5bda60c46232b2c858bf3dd060b0cc065a83f394b27';
 
 $app->group('/api', function (RouteCollectorProxy $group): void {
     $group->get('/users/[{token}]', function (Request $request, Response $response, array $args): Response {
         if (!empty($args['token'])) {
-            if ($args['token'] == API::Token) {
+            if ($args['token'] == TOKEN) {
                 $users = db_select("select * from user_tb");
                 $rows = count($users);
                 return response_json($response, array('data' => $users, 'rows' => $rows));
